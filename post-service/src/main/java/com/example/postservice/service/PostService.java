e787180416a2f259af1a42695af5184a4284335a
gyarababu <gyarababu9@gmail.com>
2022-03-23 19:22:25 +0530

    added get all

--- post-service/src/main/java/com/example/postservice/service/PostService.java
+++ post-service/src/main/java/com/example/postservice/service/PostService.java
@@ -3,10 +3,12 @@ package com.example.postservice.service;
 import com.example.postservice.model.Post;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public interface PostService {
     Post createPost(Post post);
 
     Post getPost(String postId);
-
+    List<Post> getPosts();
 }
--- post-service/src/main/java/com/example/postservice/service/PostServiceImpl.java
+++ post-service/src/main/java/com/example/postservice/service/PostServiceImpl.java
@@ -3,7 +3,12 @@ package com.example.postservice.service;
 import com.example.postservice.model.Post;
 import com.example.postservice.repo.PostRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Service
 public class PostServiceImpl implements  PostService {
@@ -19,4 +24,10 @@ public class PostServiceImpl implements  PostService {
     public Post getPost(String postId) {
         return postRepo.findById(postId).get();
     }
+    @Override
+    public List<Post> getPosts() {
+        return  postRepo.findAll();
+    }
+
+
 }
