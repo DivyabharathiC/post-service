5193acb92f983193be62c342a81d16855de80600
Divyabharathi <divyabharathic@maveric-systems.com>
2022-03-30 16:08:48 +0530

    Added ELK logs

--- post-service/src/main/java/com/example/postservice/controller/PostController.java
+++ post-service/src/main/java/com/example/postservice/controller/PostController.java
@@ -3,6 +3,9 @@ package com.example.postservice.controller;
 import com.example.postservice.dto.PostDTO;
 import com.example.postservice.model.Post;
 import com.example.postservice.service.PostService;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,11 +13,13 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Slf4j
 @RestController
 @CrossOrigin(value = "*")
 @RequestMapping(path="/api/v1/posts")
 public class PostController {
 
+    private static Logger logger = LoggerFactory.getLogger(PostController.class);
     @Autowired
     PostService postService;
 
@@ -22,28 +27,31 @@ public class PostController {
 
     public ResponseEntity<PostDTO> createPost(@RequestBody Post post)
     {
+        logger.info("Starting of createPost request from post application");
         return  new ResponseEntity<PostDTO>(postService.createPost(post), HttpStatus.CREATED);
     }
 
     @GetMapping("/{postId}")
     public ResponseEntity<PostDTO> getPost(@PathVariable("postId") String postId) {
-
+        logger.info("Starting of getPostbyid request from post application");
         return  new ResponseEntity<PostDTO>(postService.getPost(postId), HttpStatus.OK);
     }
 
     @GetMapping(path = "")
     public ResponseEntity<List<PostDTO>> getPosts() {
-
+        logger.info("Starting of getPosts request from post application");
         return  new ResponseEntity<List<PostDTO>>(postService.getPosts(), HttpStatus.OK);
     }
 
     @PutMapping(path = "/{postId}")
     public ResponseEntity<PostDTO> updatePosts(@PathVariable("postId") String postId,@RequestBody Post post){
+        logger.info("Starting of updatePosts request from post application");
         return new ResponseEntity<PostDTO>(postService.updatePosts(postId, post), HttpStatus.OK);
     }
 
     @DeleteMapping(path = "/{postId}")
     public ResponseEntity<String> deletePost(@PathVariable("postId") String postId) {
+        logger.info("Starting of deletePost request from post application");
         return new ResponseEntity<String>(postService.deletePost(postId), HttpStatus.OK);
     }
 
